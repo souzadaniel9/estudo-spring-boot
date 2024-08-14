e7886ac4b745fdba0db3d87c48e3a07e3f7c2846
Daniel <danielflorindo9@gmail.com>
2024-08-14 19:18:00 -0300

    Adicionando Suporte à Migrations com Flyway

--- FirstStepsInJavaSpringBoot/estudo-spring-boot/src/main/java/br/com/daniel/controllers/PersonController.java
+++ FirstStepsInJavaSpringBoot/estudo-spring-boot/src/main/java/br/com/daniel/controllers/PersonController.java
@@ -22,7 +22,7 @@ import br.com.daniel.data.vo.v2.PersonVOV2;
 import br.com.daniel.services.PersonServices;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 
 	@Autowired
